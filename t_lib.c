9e81730c705030c3ad182320b540cbcaddeb05f0
Ryan Geary <rtgnj42@gmail.com>
2018-10-29 21:18:06 -0400

    Set up queues?

--- t_lib.c
+++ t_lib.c
@@ -11,7 +11,8 @@ typedef struct tcb tcb;
 
 ucontext_t *running;
 ucontext_t *ready;
-
+tcb* readyQueue;
+tcb* runningQueue;
 void t_yield()
 {
   ucontext_t *tmp;
@@ -25,11 +26,18 @@ void t_yield()
 
 void t_init()
 {
+  readyQueue = (tcb*) malloc(sizeof(tcb));
+  runningQueue = (tcb*) malloc(sizeof(tcb));
   ucontext_t *tmp;
   tmp = (ucontext_t *) malloc(sizeof(ucontext_t));
 
   getcontext(tmp);    /* let tmp be the context of main() */
   running = tmp;
+  runningQueue->thread_id = 0;
+  runningQueue->thread_priority = 0;
+  runningQueue->thread_context = *tmp;
+  runningQueue->next = NULL;
+  readyQueue = NULL;
 }
 
 int t_create(void (*fct)(int), int id, int pri)
@@ -51,4 +59,14 @@ int t_create(void (*fct)(int), int id, int pri)
   uc->uc_link = running; 
   makecontext(uc, (void (*)(void)) fct, 1, id);
   ready = uc;
+}
+
+void t_shutdown() {
+  tcb* current = runningQueue;
+  tcb* next = current;
+  while(NULL != current) {
+    next = current->next;
+    free(current);
+    current = next;
+  }
 }
