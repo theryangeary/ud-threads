1c79687db4e8ac1792495959a26e0f5e29f72bb2
Ryan Geary <rtgnj42@gmail.com>
2018-11-07 16:45:29 -0500

    Add color coding for easier test result parsing

--- comprehensive_test.sh
+++ comprehensive_test.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 declare -a arr=("test00" "test01" "test01x" "test01a" "test02" "test02a") #"test03" "test04" "test05" "test06" "test07" "test08" "test09" "test10" "test11")
 
@@ -8,12 +11,16 @@ do
     x=`./$i > output && diff outcomes/$i output | wc -l`
     if [ $x != 0 ]
     then
-        echo "$i has wrong output"
+        echo -e "${RED}$i has WRONG output${NC}"
+else
+	echo -e "${GREEN}$i has correct output${NC}"
     fi
     v=`valgrind ./$i |& grep "LEAK SUMMARY" | wc -l`
     if [ $v != 0 ]
     then
-        echo "$i has memory leaks"
+        echo -e "${RED}$i has MEMORY LEAKS${NC}"
+else
+	echo -e "${GREEN}$i has no memory leaks${NC}"
     fi
 done
 echo "#############################################"
